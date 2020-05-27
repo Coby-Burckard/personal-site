b15b75585366ad4d141153bc6f81131cd0deebf5
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-05-27 13:47:02 -0500

    adding section component and styling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import store from './store/store';
 import { Provider } from 'react-redux';
 
+//https://colorlib.com/preview/#ronaldo is the example 
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
