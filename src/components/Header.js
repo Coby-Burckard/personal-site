2a92c2ac3d2b13fd79d492b848e5428e044c33ff
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2020-05-27 23:36:35 -0500

    styling the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Link, animateScroll } from 'react-scroll';
+import { Link } from 'react-scroll';
 
 export default props => {
   return (
     <div className="header">
-      <Link
+      <a
         className="header__link"
-        activeClass="header__link--active"
-        to="introduction"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
+        href="https://github.com/Coby-Burckard/personal-site"
       >
-        Introduction
-      </Link>
-      <a className="header__link" href="#education">
-        Education
+        GitHub
       </a>
+      <div className="header__navlist">
+        <Link
+          className="header__link"
+          activeClass="header__link--active"
+          to="introduction"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          Introduction
+        </Link>
+        <a className="header__link" href="#education">
+          Education
+        </a>
+      </div>
     </div>
   );
 };
