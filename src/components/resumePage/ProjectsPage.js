095018ecd30e133c69c5326aabd917f3a9e3b63d
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2021-02-06 01:28:24 -0600

    adding loading project

--- src/components/resumePage/ProjectsPage.js
+++ src/components/resumePage/ProjectsPage.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import mini from './images/mini.jpg';
-import codenames from './images/codename.jpg';
-import advent from './images/advent-of-code.jpg';
-import normal from './images/normal.jpg'
+import React from "react"
+import mini from "./images/mini.jpg"
+import codenames from "./images/codename.jpg"
+import advent from "./images/advent-of-code.jpg"
+import normal from "./images/normal.jpg"
+import loading from "./images/loading.jpg"
 
 export default () => (
   <div className="projects" id="projects">
@@ -14,19 +15,36 @@ export default () => (
           <div className="projects__info-container">
             <h3 className="projects__project-title">Codenames</h3>
             <p className="projects__project-description">
-              <strong>Note:</strong> The backend is hosted with the heroku free tier and may be slow to start up. <br/><br/>
-              A multiplayer replica of the popular game.
+              <strong>Note:</strong> The backend is hosted with the heroku free
+              tier and may be slow to start up. <br />
+              <br />A multiplayer replica of the popular game.
             </p>
           </div>
         </a>
       </div>
       <div className="card">
-        <a className="project" href="https://gaussian-distributions.netlify.app/">
+        <a
+          className="project"
+          href="https://gaussian-distributions.netlify.app/"
+        >
           <img className="projects__img" src={normal} alt="mini-projects" />
           <div className="projects__info-container">
             <h3 className="projects__project-title">Gaussian</h3>
             <p className="projects__project-description">
-              A normal distribution graphing and z-test tool.  Built with typescript, react, and styled components.
+              A normal distribution graphing and z-test tool. Built with
+              typescript, react, and styled components.
+            </p>
+          </div>
+        </a>
+      </div>
+      <div className="card">
+        <a className="project" href="https://loading-heroku.netlify.app/">
+          <img className="projects__img" src={loading} alt="mini-projects" />
+          <div className="projects__info-container">
+            <h3 className="projects__project-title">Loading</h3>
+            <p className="projects__project-description">
+              Animated loading screen for apps hosted on heroku. The animations
+              were done with react-css-transitions and keyframes in SCSS
             </p>
           </div>
         </a>
@@ -35,10 +53,13 @@ export default () => (
         <a className="project" href="https://aoc-2020-visualized.netlify.app/">
           <img className="projects__img" src={advent} alt="Advent of Code" />
           <div className="projects__info-container">
-            <h3 className="projects__project-title">Advent of Code Visualized</h3>
+            <h3 className="projects__project-title">
+              Advent of Code Visualized
+            </h3>
             <p className="projects__project-description">
-              A one day coding challenge from the popular Advent of Code. I took my
-              solution and spun up a quick react app in the afternoon of day 11.
+              A one day coding challenge from the popular Advent of Code. I took
+              my solution and spun up a quick react app in the afternoon of day
+              11.
             </p>
           </div>
         </a>
@@ -49,12 +70,14 @@ export default () => (
           <div className="projects__info-container">
             <h3 className="projects__project-title">Mini-Projects</h3>
             <p className="projects__project-description">
-              <strong>Note:</strong> The backend is hosted with the heroku free tier and may be slow to start up.<br/><br/>
-              A collection of small full stack and frontend projects.
+              <strong>Note:</strong> The backend is hosted with the heroku free
+              tier and may be slow to start up.
+              <br />
+              <br />A collection of small full stack and frontend projects.
             </p>
           </div>
         </a>
       </div>
     </div>
   </div>
-);
+)
